c4e146b81cb74e8fca80305a190402a227a2b0c8
fathi <you@example.com>
2022-01-19 03:58:49 +0200

    add url

--- vezeeta/project/urls.py
+++ vezeeta/project/urls.py
@@ -18,9 +18,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('accounts/', include('accounts.urls',namespace='accounts')),
+    path('', include('accounts.urls')),
     path('contact/', include('contact.urls',namespace='contact'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
